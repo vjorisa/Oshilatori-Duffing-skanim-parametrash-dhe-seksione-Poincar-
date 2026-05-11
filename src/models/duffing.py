09cc54d13a321af02bc04259035fa2667f34e9ab
vjorisa <vjorisac@gmail.com>
2026-05-11 13:57:08 +0200

    Implement Duffing oscillator equations and energy function

--- src/models/duffing.py
+++ src/models/duffing.py
@@ -1,1 +1,22 @@
+import numpy as np
 
+def duffing_rhs(t, y, params):
+    """
+    y[0] = x (pozicioni)
+    y[1] = v (shpejtësia)
+    """
+    x, v = y
+    delta = params['delta']
+    alpha = params['alpha']
+    beta = params['beta']
+    gamma = params['gamma']
+    omega = params['omega']
+    
+    dxdt = v
+    dvdt = gamma * np.cos(omega * t) - delta * v - alpha * x - beta * x**3
+    return [dxdt, dvdt]
+
+def effective_energy(x, v, params):
+    # E = Kinetike + Potenciale
+    # U(x) = 0.5*alpha*x^2 + 0.25*beta*x^4
+    return 0.5 * v**2 + 0.5 * params['alpha'] * x**2 + 0.25 * params['beta'] * x**4
